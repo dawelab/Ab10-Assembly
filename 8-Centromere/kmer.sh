d9b48f039e358649dd76f49227519ede32d4a7cf
jl03308 <jl03308@uga.edu>
2020-01-03 12:15:15 -0500

    Update kmer.sh

--- 8-Centromere/kmer.sh
+++ 8-Centromere/kmer.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#PBS -q highmem_q
-#PBS -N kmer
-#PBS -l nodes=1:ppn=12
-#PBS -l walltime=1:30:00
-#PBS -l mem=250gb
-#PBS -M jl03308@uga.edu
-#PBS -m ae
 
 genome="$1"
 genome_name=$(basename $genome |cut -f1 -d ".")
