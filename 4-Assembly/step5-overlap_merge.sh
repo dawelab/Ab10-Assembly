6e46617aab490246d1fe83149adcea6019a05510
jl03308 <jl03308@uga.edu>
2020-01-02 20:03:06 -0500

    Update step5-overlap_merge.sh

--- 4-Assembly/step5-overlap_merge.sh
+++ 4-Assembly/step5-overlap_merge.sh
@@ -4,10 +4,7 @@ module load Biopython/1.71-foss-2018a-Python-3.6.4
 xmap=$1
 key=$2
 out_dir=$3
-python Overlap_merge.py -x ./alignref/core_sup_merged_hybridassembly0_CTTAAG_0kb_0labels.xmap \
--k ./core_sup_merged_hybridassembly0_CTTAAG_0kb_0labels_key.txt \
--o ./overlap_merge_src
+python Overlap_merge.py -x $xmap \
+-k $key \
+-o $out_dir
 
-singularity exec /usr/local/singularity-images/seqkit-0.10.1.simg seqkit grep -v -f modified_seq.txt core_sup_merged_hybridassembly0.fasta > core_sup_merged_hybridassembly0.tmp.fasta
-tmp=$(head -1 ${output_dir}/output_seq.txt)
-cat core_sup_merged_hybridassembly0.tmp.fasta $tmp > core_sup_merged_hybridassembly0.rmoverlap.fasta
